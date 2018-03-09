973771e47082198bdfba6cc00dafad6e00acd859
Lincoln Jones <lincoln.jones@2-com.net>
2018-03-08 19:37:01 -0600

    working server from class

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-//This is not working yet
-
 // grab the packages we need
 var express = require('express');
 var app = express();
@@ -9,7 +7,7 @@ var url = "mongodb://localhost:27017/";
 
 // routes will go here
 app.get('/', function(req, res) {
-	res.sendfile('/Users/lincolnjones/Desktop/WebAppNewsPage-master/I Love News/iLoveNewsFP.html')
+	res.sendfile('/Users/lincolnjones/Desktop/node_form_mongo/index.html')
 })
 
 app.get('/create/', function(req, res) {
@@ -57,32 +55,6 @@ app.get('/login/', function(req, res) {
 		});
 	});
 
-	res.send(response);
-});
-
-app.get('/news/', function(req, res) {
-	var t = req.query.title;
-	var a = req.query.text_form;
-
-	MongoClient.connect(url, function(err, db) {
-		if (err) throw err;
-		var dbo = db.db("news");
-		var myobj = { title: t, article: a };
-		dbo.collection("articles").insertOne(myobj, function(err, res) {
-			if (err) 
-			{
-				throw err;
-				db.close;
-				res.send("Error. Article not submitted.");
-			}
-			else 
-			{
-				console.log("1 document inserted.");
-				db.close;
-				res.send("Article successfully submitted.");
-			}
-		});
-	});
 	res.send(response);
 });
 
